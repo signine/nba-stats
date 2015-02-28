70ff723317b650c8a1f94e459a88e0cf49407ed1
Shamil Ishraq <shamil.ishraq@gmail.com>
2015-02-28 01:25:00 -0500

    scoreboard

--- lib/nba.rb
+++ lib/nba.rb
@@ -7,9 +7,10 @@ class NBA
 
   base_uri "http://stats.nba.com/stats/"
 
+  BOXSCORE_BASE = "http://www.nba.com/games"
+  BOXSCORE_END = "gameinfo.html"
   DEFAULT_OPTS = {"LeagueID" => "00", "DayOffset" => 0}
   ENDPOINTS = {scoreboard: '/scoreboard'}
-  # http://stats.nba.com/stats/scoreboard?LeagueID=00&DayOffset=0&GameDate=01/22/2015
 
   def get_scoreboard date
     raise InvalidArgument unless date || date.kind_of?(Date)
@@ -17,14 +18,47 @@ class NBA
     opts = DEFAULT_OPTS.merge({"GameDate" => date.strftime('%m/%d/%Y')})
     response = self.class.get(ENDPOINTS[:scoreboard], query: opts)
 
-    if valid? response
-      JSON.parse response.body
-    else
-      raise "Error code returned: #{response.code}"
-    end
+    raise "Error code returned: #{response.code}" unless valid? response
+
+    data = JSON.parse response.body
+    games = parse_games data
+
+    games
   end
 
   private
+
+  def parse_games data
+    games = []
+
+    data['resultSets'][0]['rowSet'].each do |g|
+      game = {}
+      game[:id] = g[2]
+      game[:status] = g[4]
+      game[:time] = g[10]
+      game[:boxscore_link] = make_boxscore_link(g[5]) 
+
+      line_scores = get_linescores data, game[:id]
+
+      game[:team_1] = line_scores[0][4]
+      game[:team_1_score] = line_scores[0][21]
+
+      game[:team_2] = line_scores[1][4]
+      game[:team_2_score] = line_scores[1][21]
+
+      games << game
+    end
+
+    games
+  end
+
+  def make_boxscore_link game_code
+    "#{BOXSCORE_BASE}/#{game_code}/#{BOXSCORE_END}"
+  end
+
+  def get_linescores data, game_id
+    data['resultSets'][1]['rowSet'].select { |g| g[2] == game_id }
+  end
 
   def valid? resp
     resp.code == 200
